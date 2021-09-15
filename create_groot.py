9a55c500cf411d8108b7117eb6c709b9c75d2d52
ehong2048 <ehong2048@gmail.com>
2021-09-15 12:14:20 -0700

    started create_groot.py file and added imports

---
+++ create_groot.py
@@ -0,0 +1,9 @@
+import numpy as np
+import argparse
+import cv2
+
+"""
+Functionality: loads and displays Groot image that is cropped to be just Groot's outline
+use thresholding and mask on image 
+"""
+
